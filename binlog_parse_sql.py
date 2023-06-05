7b6dbeb235e0c7c7d4093c0b0d352a51a9eba90f
贺春旸 <chunyang_he@139.com>
2023-06-05 12:18:44 +0800

    Add files via upload

--- binlog_parse_sql.py
+++ binlog_parse_sql.py
@@ -8,6 +8,9 @@ from pymysqlreplication.row_event import (
     UpdateRowsEvent,
     DeleteRowsEvent
 )
+from pymysqlreplication.event import QueryEvent
+from pymysqlreplication.event import GtidEvent
+
 
 # 源 MySQL 数据库设置
 source_mysql_settings = {
@@ -56,8 +59,7 @@ def load_binlog_pos():
 
     return binlog_file, int(binlog_pos)
 
-
-# 在程序退出时保存当前的 binlog 文件名和位置点
+# 退出程序时保存当前的 binlog 文件名和位置点
 def exit_handler(stream, current_binlog_file, binlog_pos):
     stream.close()
     save_binlog_pos(current_binlog_file, binlog_pos)
@@ -83,7 +85,8 @@ stream = BinLogStreamReader(
     server_id=source_server_id,  
     blocking=True,
     resume_stream=True,
-    only_events=[WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent],
+    #only_events=[WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent],
+    only_events=[WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent, QueryEvent],
     log_file=saved_pos[0],
     log_pos=saved_pos[1]
 )
@@ -94,55 +97,66 @@ def process_rows_event(binlogevent, stream):
         database_name = binlogevent.schema  # 获取数据库名
     else:
         database_name = None
-    for row in binlogevent.rows:
-        if isinstance(binlogevent, WriteRowsEvent):
-            sql = "INSERT INTO {}({}) VALUES ({})".format(
-                f"{database_name}.{binlogevent.table}" if database_name else binlogevent.table,
-                '`' + '`,`'.join(list(row["values"].keys())) + '`',
-                ','.join(["'%s'" % str(i) for i in list(row["values"].values())])
-            )
-            print(sql)
-            try:
-                with target_conn.cursor() as cursor:
-                    cursor.execute(sql)
-                    target_conn.commit()
-            except Exception as e:
-                print(f"Failed to execute SQL: {sql}")
-                print(f"Error message: {e}")
+    
+    if isinstance(binlogevent, QueryEvent):
+        sql = binlogevent.query
+        try:
+            with target_conn.cursor() as cursor:
+                cursor.execute(sql)
+                target_conn.commit()
+                print(f"success to execute SQL: {sql}")
+        except Exception as e:
+            print(f"Failed to execute SQL: {sql}")
+            print(f"Error message: {e}")
+    else:
+        for row in binlogevent.rows:
+            if isinstance(binlogevent, WriteRowsEvent):
+                sql = "INSERT INTO {}({}) VALUES ({})".format(
+                    f"{database_name}.{binlogevent.table}" if database_name else binlogevent.table,
+                    '`' + '`,`'.join(list(row["values"].keys())) + '`',
+                    ','.join(["'%s'" % str(i) for i in list(row["values"].values())])
+                )
+                print(sql)
+                try:
+                    with target_conn.cursor() as cursor:
+                        cursor.execute(sql)
+                        target_conn.commit()
+                except Exception as e:
+                    print(f"Failed to execute SQL: {sql}")
+                    print(f"Error message: {e}")
+    
+            elif isinstance(binlogevent, UpdateRowsEvent):
+                sql = "UPDATE {} SET {} WHERE {}".format(
+                    f"{database_name}.{binlogevent.table}" if database_name else binlogevent.table,
+                    ','.join(["`{}`='{}'".format(k, v) for k, v in row["after_values"].items()]),
+                    ' AND '.join(["`{}`='{}'".format(k, v) for k, v in row["before_values"].items()])
+                )
+                print(sql)
+    
+                try:
+                    with target_conn.cursor() as cursor:
+                       cursor.execute(sql)
+                       target_conn.commit()
+                except Exception as e:
+                    print(f"Failed to execute SQL: {sql}")
+                    print(f"Error message: {e}")
+    
+            elif isinstance(binlogevent, DeleteRowsEvent):
+                sql = "DELETE FROM {} WHERE {}".format(
+                    f"{database_name}.{binlogevent.table}" if database_name else binlogevent.table,
+                    ' AND '.join(["`{}`='{}'".format(k, v) for k, v in row["values"].items()])
+                )
+                print(sql)
+    
+                try:
+                    with target_conn.cursor() as cursor:
+                        cursor.execute(sql)
+                        target_conn.commit()
+                except Exception as e:
+                    print(f"Failed to execute SQL: {sql}")
+                    print(f"Error message: {e}")
 
-        elif isinstance(binlogevent, UpdateRowsEvent):
-            sql = "UPDATE {} SET {} WHERE {}".format(
-                f"{database_name}.{binlogevent.table}" if database_name else binlogevent.table,
-                ','.join(["`{}`='{}'".format(k, v) for k, v in row["after_values"].items()]),
-                ' AND '.join(["`{}`='{}'".format(k, v) for k, v in row["before_values"].items()])
-            )
-            print(sql)
-
-            try:
-                with target_conn.cursor() as cursor:
-                   cursor.execute(sql)
-                   target_conn.commit()
-            except Exception as e:
-                print(f"Failed to execute SQL: {sql}")
-                print(f"Error message: {e}")
-
-        elif isinstance(binlogevent, DeleteRowsEvent):
-            sql = "DELETE FROM {} WHERE {}".format(
-                f"{database_name}.{binlogevent.table}" if database_name else binlogevent.table,
-                ' AND '.join(["`{}`='{}'".format(k, v) for k, v in row["values"].items()])
-            )
-            print(sql)
-
-            try:
-                with target_conn.cursor() as cursor:
-                    cursor.execute(sql)
-                    target_conn.commit()
-            except Exception as e:
-                print(f"Failed to execute SQL: {sql}")
-                print(f"Error message: {e}")
-
-    return binlogevent.packet.log_pos
-
+        return binlogevent.packet.log_pos
 
 # 循环遍历解析出来的行事件并存入SQL语句中
 while True:

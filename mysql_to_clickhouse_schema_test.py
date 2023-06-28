d0933095b9b986b9fa7a1dbeebd0c0a0042392b9
贺春旸 <chunyang_he@139.com>
2023-06-28 14:10:34 +0800

    MySQL表结构转换为ClickHouse表结构，该工具仅为单库单表测试使用
    MySQL表结构转换为ClickHouse表结构，该工具仅为单库单表测试使用

--- mysql_to_clickhouse_schema_test.py
+++ mysql_to_clickhouse_schema_test.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 # MySQL表结构转换为ClickHouse表结构，该工具仅为单库单表测试使用
+
 import pymysql
 import re
 from clickhouse_driver import Client
 
+#################修改以下配置配置信息#################
+# MySQL数据库配置
+MYSQL_HOST = "192.168.198.239"
+MYSQL_PORT = 3336
+MYSQL_USER = "admin"
+MYSQL_PASSWORD = "hechunyang"
+MYSQL_DATABASE = "hcy"
+
+# ClickHouse数据库配置
+CLICKHOUSE_HOST = "192.168.176.204"
+CLICKHOUSE_PORT = 9000
+CLICKHOUSE_USER = "hechunyang"
+CLICKHOUSE_PASSWORD = "123456"
+CLICKHOUSE_DATABASE = "hcy"
+
+# 要操作的表名
+TABLE_NAME = "hcy"
+
+#################以下代码不用修改#################
 def convert_field_type(field_type):
     """
     将MySQL字段类型转换为ClickHouse字段类型
@@ -24,7 +44,9 @@ def convert_field_type(field_type):
     elif "double" in field_type or "numeric" in field_type:
         return "Float64"
     elif "decimal" in field_type:
-        return "Decimal128(2)"
+        precision_scale = re.search(r'\((.*?)\)', field_type).group(1)
+        precision, scale = precision_scale.split(',')
+        return f"Decimal({precision}, {scale})"
     elif "datetime" in field_type or "timestamp" in field_type or "date" in field_type:
         return "DateTime"
     elif "char" in field_type or "varchar" in field_type or "text" in field_type or "enum" in field_type or "set" in field_type:
@@ -38,7 +60,7 @@ def convert_field_type(field_type):
     elif "varbinary" in field_type:
         return "String"
     elif field_type.startswith("bit"):
-        return "UInt8"        
+        return "UInt8"
     else:
         raise ValueError(f"无法转化未知 MySQL 字段类型：{field_type}")
 
@@ -62,6 +84,7 @@ def convert_mysql_to_clickhouse(mysql_conn, mysql_database, mysql_table, clickho
         column_type = mysql_column[1]
 
         # 转换字段类型
+        #print(f"column_type: {column_type}")
         clickhouse_type = convert_field_type(column_type)
 
         # 拼接SQL语句
@@ -90,15 +113,22 @@ def convert_mysql_to_clickhouse(mysql_conn, mysql_database, mysql_table, clickho
 if __name__ == "__main__":
     # 连接MySQL数据库
     mysql_conn = pymysql.connect(
-        host="192.168.198.239",
-        port=3336,
-        user="admin",
-        password="hechunyang",
-        database="hcy"
+        host=MYSQL_HOST,
+        port=MYSQL_PORT,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DATABASE
     )
 
     # 连接ClickHouse数据库
-    clickhouse_conn = Client(host='192.168.176.204',port=9000,user='hechunyang',password='123456',database='hcy')
+    clickhouse_conn = Client(
+        host=CLICKHOUSE_HOST,
+        port=CLICKHOUSE_PORT,
+        user=CLICKHOUSE_USER,
+        password=CLICKHOUSE_PASSWORD,
+        database=CLICKHOUSE_DATABASE
+    )
 
     # 转化表结构（将MySQL的hcy库的user表 转换为 ClickHouse的hcy库的user表）
-    convert_mysql_to_clickhouse(mysql_conn, "hcy", "user", clickhouse_conn, "hcy")
+    convert_mysql_to_clickhouse(mysql_conn, MYSQL_DATABASE, TABLE_NAME, clickhouse_conn, CLICKHOUSE_DATABASE)
+    

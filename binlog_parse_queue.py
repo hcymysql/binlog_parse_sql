00656bf4037f0ed90c3c514102c55015e291b87a
贺春旸 <chunyang_he@139.com>
2023-12-13 14:22:14 +0800

    适用于将MySQL8.0迁移至MariaDB（ETL抽数据工具）
    适用于将MySQL8.0迁移至MariaDB（ETL抽数据工具）

--- binlog_parse_queue.py
+++ binlog_parse_queue.py
@@ -5,7 +5,7 @@ import os
 import pymysql
 import signal
 import atexit
-import time
+import time, datetime
 from queue import Queue
 from threading import Thread
 from pymysqlreplication import BinLogStreamReader
@@ -16,7 +16,6 @@ from pymysqlreplication.row_event import (
 )
 from pymysqlreplication.event import QueryEvent
 from pymysqlreplication.event import GtidEvent
-
 
 #################修改以下配置配置信息#################
 # 源 MySQL 8.0 数据库设置
@@ -29,10 +28,10 @@ source_mysql_settings = {
     "charset": "utf8mb4",
 }
 
-#设置源MySQL Server-Id
+# 设置源MySQL Server-Id
 source_server_id = 413336
 
-#设置从源同步的binlog文件名和位置点，默认值为 mysql-bin.000001 和 4
+# 设置从源同步的binlog文件名和位置点，默认值为 mysql-bin.000001 和 4
 binlog_file = "mysql-bin.000001"
 binlog_pos = 4
 
@@ -45,6 +44,7 @@ target_mysql_settings = {
     "database": "hcy",
     "charset": "utf8"
 }
+
 
 #################以下代码不用修改#################
 
@@ -59,6 +59,7 @@ def save_binlog_pos(binlog_file, binlog_pos):
         with open('binlog_info.txt', 'w') as f:
             f.write('{}\n{}'.format(current_binlog_file, binlog_pos))
         print('Binlog position ({}, {}) updated.'.format(current_binlog_file, binlog_pos))
+
 
 # 读取上次保存的 binlog 位置和文件名
 def load_binlog_pos():
@@ -78,19 +79,22 @@ def load_binlog_pos():
 # 退出程序时保存当前的 binlog 文件名和位置点
 def exit_handler(stream, current_binlog_file, binlog_pos):
     stream.close()
-    #save_binlog_pos(current_binlog_file, binlog_pos)
+    # save_binlog_pos(current_binlog_file, binlog_pos)
     save_binlog_pos(current_binlog_file, binlog_pos or stream.log_pos)
+
 
 # 在程序被终止时保存当前的 binlog 文件名和位置点
 def save_binlog_pos_on_termination(signum, frame):
     save_binlog_pos(current_binlog_file, binlog_pos or stream.log_pos)
     quit_program()
 
+
 # 退出程序时保存当前的 binlog 文件名和位置点
 def quit_program():
     stream.close()
     target_conn.close()
     exit(0)
+
 
 # 建立连接
 target_conn = pymysql.connect(**target_mysql_settings)
@@ -99,6 +103,7 @@ saved_pos = load_binlog_pos()
 
 # 定义队列用于存放 SQL 语句并作为解析 binlog 和执行 SQL 语句的中间件
 sql_queue = Queue()
+
 
 # 定义 SQL 执行函数，从队列中取出 SQL 语句并依次执行
 def sql_worker():
@@ -118,19 +123,21 @@ def sql_worker():
         finally:
             sql_queue.task_done()
 
+
 # 启动 SQL 执行线程
 sql_thread = Thread(target=sql_worker, daemon=True)
 sql_thread.start()
 
 stream = BinLogStreamReader(
     connection_settings=source_mysql_settings,
-    server_id=source_server_id,  
+    server_id=source_server_id,
     blocking=True,
     resume_stream=True,
-    #only_events=[WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent, QueryEvent],
+    # only_events=[WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent, QueryEvent],
     log_file=saved_pos[0],
     log_pos=saved_pos[1]
 )
+
 
 # 循环遍历解析出来的行事件并存入SQL语句中
 def process_rows_event(binlogevent, stream):
@@ -149,50 +156,52 @@ def process_rows_event(binlogevent, stream):
     else:
         for row in binlogevent.rows:
             if isinstance(binlogevent, WriteRowsEvent):
-                sql = "INSERT INTO {}({}) VALUES ({})".format(
-                    f"{database_name}.{binlogevent.table}" if database_name else binlogevent.table,
-                    '`' + '`,`'.join(list(row["values"].keys())) + '`',
-                    ','.join(["'%s'" % str(i) for i in list(row["values"].values())])
+                sql = "INSERT INTO {}({}) VALUES ({});".format(
+                    f"`{database_name}`.`{binlogevent.table}`" if database_name else binlogevent.table,
+                    ','.join(["`{}`".format(k) for k in row["values"].keys()]),
+                    ','.join(["'{}'".format(v) if isinstance(v, (
+                        str, datetime.datetime, datetime.date)) else 'NULL' if v is None else str(v)
+                              for v in row["values"].values()])
                 )
                 print(sql)
                 sql_queue.put(sql)  # 将 SQL 语句加入队列
-    
+
             elif isinstance(binlogevent, UpdateRowsEvent):
-                '''
-                sql = "UPDATE {} SET {} WHERE {}".format(
-                    f"{database_name}.{binlogevent.table}" if database_name else binlogevent.table,
-                    ','.join([f"`{k}`={'NULL' if v is None else f'{v}'}" for k, v in row["after_values"].items()]),
-                    ' AND '.join(["`{}`='{}'".format(k, v) for k, v in row["before_values"].items()])
-                )
-                '''
                 set_values = []
                 for k, v in row["after_values"].items():
                     if isinstance(v, str):
                         set_values.append(f"`{k}`='{v}'")
+                    elif isinstance(v, (datetime.datetime, datetime.date)):
+                        set_values.append(f"`{k}`='{v}'")  # 将时间字段转换为字符串形式
                     else:
-                        set_values.append(f"`{k}`={v}")
+                        set_values.append(f"`{k}`={v}" if v is not None else f"`{k}`= NULL")
                 set_clause = ','.join(set_values)
 
                 where_values = []
                 for k, v in row["before_values"].items():
                     if isinstance(v, str):
                         where_values.append(f"`{k}`='{v}'")
+                    elif isinstance(v, (datetime.datetime, datetime.date)):
+                        where_values.append(f"`{k}`='{v}'")  # 添加对时间类型的处理
                     else:
-                        where_values.append(f"`{k}`={v}")
+                        where_values.append(f"`{k}`={v}" if v is not None else f"`{k}` IS NULL")
                 where_clause = ' AND '.join(where_values)
 
-                sql = f"UPDATE {database_name}.{binlogevent.table} SET {set_clause} WHERE {where_clause}"
+                sql = f"UPDATE `{database_name}`.`{binlogevent.table}` SET {set_clause} WHERE {where_clause};"
                 print(sql)
                 sql_queue.put(sql)  # 将 SQL 语句加入队列
-    
+
             elif isinstance(binlogevent, DeleteRowsEvent):
-                sql = "DELETE FROM {} WHERE {}".format(
-                    f"{database_name}.{binlogevent.table}" if database_name else binlogevent.table,
-                    ' AND '.join(["`{}`='{}'".format(k, v) for k, v in row["values"].items()])
+                sql = "DELETE FROM {} WHERE {};".format(
+                    f"`{database_name}`.`{binlogevent.table}`" if database_name else binlogevent.table,
+                    ' AND '.join(
+                        ["`{}`={}".format(k, "'{}'".format(v) if isinstance(v, (str, datetime.datetime, datetime.date))
+                        else 'NULL' if v is None else str(v))
+                         for k, v in row["values"].items()])
                 )
                 print(sql)
                 sql_queue.put(sql)  # 将 SQL 语句加入队列
-                    
+
     return binlogevent.packet.log_pos
 
 
@@ -200,11 +209,10 @@ def process_rows_event(binlogevent, stream):
 while True:
     try:
         for binlogevent in stream:
-            #print(f'binlog: {stream.log_file}, positon: {stream.log_pos}')
+            # print(f'binlog: {stream.log_file}, positon: {stream.log_pos}')
             current_binlog_file = stream.log_file
             try:
                 binlog_pos = process_rows_event(binlogevent, stream)
-                #print(f'binlog_pos :=====> {binlog_pos}')
             except AttributeError as e:
                 save_binlog_pos(current_binlog_file, binlog_pos)
             else:
